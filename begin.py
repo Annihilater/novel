18a7a824301b81ffb2ee80144c1f6cf582a1f9a5
Annihilater <yanmiexingkong@gmail.com>
2019-11-22 21:44:08 +0800

    feat:run scrapy spider in pycharm

---
+++ begin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/11/22 19:48
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : begin.py
+
+from scrapy import cmdline
+
+cmdline.execute('scrapy crawl biquge'.split())
